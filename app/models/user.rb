b72be586d918d24e0ac0a47ae8648812e131d010
saw-code <saw.coding@gmail.com>
2021-09-29 16:55:36 +0300

    add new validations and authetication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,50 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  # параметр работы модуля шифрования паролей
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
   has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # создаем т.н. "соль" рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хэш пароля - длинная уникальная строка, из к-й невозможно
+      # восстановить исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сперва находим кандидата по email
+
+    # ОБРАТИТЕ внимание: сравнивается password_hash, а оригинальный пароль так никогда
+    # и не сохраняется нигде
+    if user.present? && user.password_hash == User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+    )
+      user
+    else
+      nil
+    end
+  end
 end
